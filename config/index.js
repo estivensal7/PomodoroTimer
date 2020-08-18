2fde7349952b2318a29f8a695617a07098c91efe
estivensal7 <estivensal7@gmail.com>
2020-08-17 20:51:51 -0400

    updated app.js to serve static files when in production

--- config/index.js
+++ config/index.js
@@ -3,7 +3,6 @@ let dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-	PORT: process.env.PORT,
 	MONGO_URI: process.env.MONGO_URI,
 	MONGO_DB_NAME: process.env.MONGO_DB_NAME,
 	JWT_SECRET: process.env.JWT_SECRET,
