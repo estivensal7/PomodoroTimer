f4362180841b179e97978bcd18dc00390d7e7f0a
estivensal7 <estivensal7@gmail.com>
2020-08-17 21:37:16 -0400

    updating, testng for deployment erros

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-let app = require("./app");
-let config = require("./config");
-
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-	console.log(`Server started on PORT ${PORT}`);
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(
+	process.env.MONGODB_URI ||
+		"mongodb+srv://graphqlUser:Test123@cluster0-uq6pl.mongodb.net/pomodoro-timer?retryWrites=true",
+	{ useNewUrlParser: true }
+);
+
+// Start the API server
+app.listen(PORT, function () {
+	console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
